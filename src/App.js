ddb23aae87d4108fd35504afdf91beb3a69d58ee
Tejas Parkar <tparkar0@gmail.com>
2020-07-15 00:38:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+import {BrowserRouter, Route ,Switch} from 'react-router-dom';
+import Home from './components/Home'
+import AboutUs from './components/AboutUs'
+import Contact from './components/Contact';
+import Posts from './components/Posts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends Component {
+ 
+  render() {
+    return (
+      <BrowserRouter>
+      <div className="App">
+        <Navbar/>
+        <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/about' component={AboutUs}/>
+        <Route path='/contact' component={Contact}/>
+        <Route path='/:post_id' component={Posts}/>
+        </Switch>
+        
+      </div>
+      </BrowserRouter>
+      
+    )
+  }
 }
 
-export default App;
+export default App
